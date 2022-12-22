aa615dc1f4fe11322b47b62a9b70d6e9d72a6aaf
Sky1ng <56270073+Sky1ng@users.noreply.github.com>
2022-12-22 17:57:29 +0100

    Ajout affichages de bien
    afficher tous les biens en fonction de la categorie passé en URL

---
+++ src/Controller/AnnonceController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Bien;
+use App\Entity\Annonce;
+
+use App\Entity\Categorie;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AnnonceController extends AbstractController
+{
+    #[Route('/annonce/{categorie}', name: 'app_annonce')]
+    public function index(string $categorie, EntityManagerInterface $entityManager): Response
+    {
+        $categoriebis = $entityManager->getRepository(Categorie::class)->findOneBy(['nom' => $categorie]);
+
+        #check si la catégorie exsite, si elle existe on renvoie un tableau avec tous les biens correspondant a ce type de bien
+        #SELECT * FROM `bien` WHERE categorie_id = (SELECT id FROM `categorie` where nom = "Terrain Agricole");
+        $bienRepository = $entityManager->getRepository(Bien::class);
+        $bien = $bienRepository->findBy(['categorie' => $categoriebis->getId()]);
+
+
+
+        return $this->render('annonce/index.html.twig', [
+            'controller_name' => 'AnnonceController','biens' => $bien
+        ]);
+    }
+}
